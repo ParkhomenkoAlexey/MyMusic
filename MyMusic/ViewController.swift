6f2ee7c7d597effd3d2ef2e606fe221d159063e0
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-08-04 17:16:33 +0300

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Алексей Пархоменко on 04/08/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
